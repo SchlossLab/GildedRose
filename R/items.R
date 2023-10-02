e15da3acb282dc1374818a89089cd0e613cd8180
amason30 <masonar@umich.edu>
2023-10-02 15:07:07 -0400

    add documentation

--- R/items.R
+++ R/items.R
@@ -1,3 +1,14 @@
+#' create items class
+#'
+#' @param name A name for the item
+#' @param sell_in A numeric denoting sell by in days
+#' @param quality quality
+#'
+#' @return An items object
+#' @export
+#'
+#' @examples
+#' new_item <- item("laptop", 0, 0)
 item <- function(name, sell_in, quality) {
   newItem <- list(name=name, sell_in=sell_in, quality=quality)
   class(newItem) <- 'item'
