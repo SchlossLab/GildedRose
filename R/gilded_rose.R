c68004f369e0895e9e799a4e40b2f8e300cfb05b
amason30 <masonar@umich.edu>
2023-10-16 14:52:39 -0400

    add logic for backtage passes and general items

--- R/gilded_rose.R
+++ R/gilded_rose.R
@@ -20,60 +20,40 @@ update_quality <- function(item){
     }
 
     item$quality <- ifelse(item$quality > 50, 50, item$quality)
+    item$quality <- ifelse(item$quality < 0, 0, item$quality)
     item$sell_in <- item$sell_in - 1
 
   } else if (item$name == "Sulfuras, Hand of Ragnaros") {
 
     item$quality <- 80
 
+  } else if (item$name == "Backstage passes to a TAFKAL80ETC concert") {
+
+    if (item$sell_in > 10) {
+      item$quality <- item$quality + 1
+    } else if (item$sell_in > 5) {
+      item$quality <- item$quality + 2
+    } else if (item$sell_in > 0) {
+      item$quality <- item$quality + 3
+    } else {
+      item$quality <- 0
+    }
+
+    item$quality <- ifelse(item$quality < 0, 0, item$quality)
+    item$sell_in <- item$sell_in - 1
+
   } else {
 
-    if (item$name != "Aged Brie" && item$name != "Backstage passes to a TAFKAL80ETC concert") {
-      if (item$quality > 0) {
-        if (item$name != "Sulfuras, Hand of Ragnaros") {
-          item$quality <- item$quality - 1
-        }
-      }
+    if (item$sell_in > 0) {
+      item$quality <- item$quality - 1
     } else {
-      if (item$quality < 50) {
-        item$quality <- item$quality + 1
-        if (item$name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item$sell_in < 11) {
-            if (item$quality < 50) {
-              item$quality = item$quality + 1
-            }
-          }
-          if (item$sell_in < 6) {
-            if (item$quality < 50) {
-              item$quality = item$quality + 1
-            }
-          }
-        }
-      }
+      item$quality <- item$quality - 2
     }
+    item$sell_in <- item$sell_in - 1
 
-    if (item$name != "Sulfuras, Hand of Ragnaros") {
-      item$sell_in <- item$sell_in - 1
-    }
-
-    if (item$sell_in < 0) {
-      if (item$name != "Aged Brie") {
-        if (item$name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (item$quality > 0) {
-            if (item$name != "Sulfuras, Hand of Ragnaros") {
-              item$quality <- item$quality - 1
-            }
-          }
-        } else {
-          item$quality <- item$quality - item$quality
-        }
-      } else {
-        if (item$quality < 50) {
-          item$quality <- item$quality + 1
-        }
-      }
-    }
+    item$quality <- ifelse(item$quality < 0, 0, item$quality)
   }
+
   item
 
 }
